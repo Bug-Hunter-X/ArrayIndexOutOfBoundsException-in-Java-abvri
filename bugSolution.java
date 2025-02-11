2908480f08a1affae65eb3a6c4b41c9c64e52cc6
BugHunter-X <askzenai@gmail.com>
2025-02-11 11:18:04 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        if (0 <= 4 && 4 < array.length) { 
+            array[4] = 10; //Corrected: Accessing valid index
+        }
+    }
+}
