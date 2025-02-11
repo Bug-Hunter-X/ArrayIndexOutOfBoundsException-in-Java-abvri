a43da296568d4f1e97f44d15d8ed2b0c2cc50e7a
BugHunter-X <askzenai@gmail.com>
2025-02-11 11:18:03 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        array[5] = 10; //This will throw an ArrayIndexOutOfBoundsException
+    }
+}
